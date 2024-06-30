118569d0db2fc09d8dd9703e120c0cdfb2e5e83d
Harsh Dixit <99479144+dixi225@users.noreply.github.com>
2024-06-30 05:09:19 -0400

    Time: 82 ms (52.63%), Space: 54.6 MB (88.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -4,15 +4,14 @@
 function moveZeroes(nums: number[]): void {
     let n=nums.length
     let i=0
+    let sizeOfZero=0
     while(i<n){
-        while(i<n&&nums[i]!=0) i++
-        if(i==n) return
-        let j=i+1
-        while(j<n&&nums[j]==0) j++
-        if(j==n) return
-        let temp=nums[j]
-        nums[j]=nums[i]
-        nums[i]=temp
+        if(nums[i]==0) sizeOfZero++
+        else if(sizeOfZero>0){
+            let temp=nums[i]
+            nums[i]=nums[i-sizeOfZero]
+            nums[i-sizeOfZero]=temp
+        }
         i++
     }
 };
