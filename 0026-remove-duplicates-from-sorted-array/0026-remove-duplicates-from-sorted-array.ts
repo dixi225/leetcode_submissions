e9d00ce5331ab6ac619071b4073174f1f79c81de
Harsh Dixit <99479144+dixi225@users.noreply.github.com>
2024-07-14 17:59:19 +0530

    Time: 67 ms (67.42%), Space: 52.9 MB (48.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,15 +1,20 @@
 function removeDuplicates(nums: number[]): number {
-    let n=nums.length
-    if(n<=1) return 1
+    if(nums.length==0) return 0
+    if(nums.length==1) return 1
     let i=1
     let j=1
-    let toCheck=nums[1]
-    while(i<n){
-        if(nums[i]!=nums[i-1]){
-        nums[j]=nums[i]
-        j++
-        }
+    let n=nums.length
+    let toChange=nums[0];
+    while(j<n){
+        while(j<n&&nums[j]<=toChange) j++
+        if(j>=n) break
+        nums[i]=nums[j]
+        toChange=nums[j]
         i++
+        j++
     }
-    return j
+    return i
+
+
+
 };
