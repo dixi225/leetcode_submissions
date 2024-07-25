2e1e35bd764b23c27619a9dab981fe3f67f4d728
Harsh Dixit <99479144+dixi225@users.noreply.github.com>
2024-07-25 07:11:59 +0530

    Time: 204 ms (77.38%), Space: 67.6 MB (95.12%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.ts
+++ 0948-sort-an-array/0948-sort-an-array.ts
@@ -1,5 +1,40 @@
-
-
 function sortArray(nums: number[]): number[] {
-    return nums.sort((a,b)=>a-b)
+    let temp: number[] = new Array(nums.length);
+    mergeSort(nums, temp, 0, nums.length - 1);
+    return nums;
 };
+
+function mergeSort(nums: number[], temp: number[], left: number, right: number): void {
+    if (left < right) {
+        let mid: number = Math.floor((left + right) / 2);
+        mergeSort(nums, temp, left, mid);
+        mergeSort(nums, temp, mid + 1, right);
+        merge(nums, temp, left, mid, right);
+    }
+}
+
+function merge(nums: number[], temp: number[], left: number, mid: number, right: number): void {
+    let i: number = left;
+    let j: number = mid + 1;
+    let k: number = left;
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j]) {
+            temp[k++] = nums[i++];
+        }
+        else {
+            temp[k++] = nums[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = nums[i++];
+    }
+
+    while (j <= right) {
+        temp[k++] = nums[j++];
+    }
+
+    for (let m: number = left; m <= right; m++) {
+        nums[m] = temp[m];
+    }
+}
