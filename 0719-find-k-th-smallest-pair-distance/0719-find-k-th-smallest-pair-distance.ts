4a10f74aea18f5332fd4f7135a78f2c08fda7791
Harsh Dixit <99479144+dixi225@users.noreply.github.com>
2024-08-14 22:57:15 +0530

    Time: 75 ms (25%), Space: 51.7 MB (50%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.ts
@@ -0,0 +1,23 @@
+function smallestDistancePair(nums: number[], k: number): number {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    let left = 0,
+        right = nums[n - 1] - nums[0];
+    while (left < right) {
+        let mid = (left + right) >> 1;
+        let count = 0,
+            i = 0;
+        for (let j = 0; j < n; j++) {
+            while (nums[j] - nums[i] > mid) {
+                i++;
+            }
+            count += j - i;
+        }
+        if (count >= k) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
